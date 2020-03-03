2110dc39f9918877d1bff80ff9bca95f7a728f85
Dror Cohen <gangu1307@gmail.com>
2020-03-03 19:44:22 +0200

    Added InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask MouseInputMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    public void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, Mathf.Infinity, MouseInputMask))
+            {
+                Vector3 position = hit.point - transform.position;
+                print(position);
+            }
+        }
+    }
+}

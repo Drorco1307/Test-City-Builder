4637425ac77138f1b2b2c88104deaa7a882c73b7
Dror Cohen <gangu1307@gmail.com>
2020-03-08 19:12:50 +0200

    Switching between selection and building state part 1

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Action OnBuildAreaHandler { get; set; }
+    public Action OnCancelActionHandler { get; set; }
+    public Button BuildResidentialAreaBtn;
+    public Button CancelActionBtn;
+    public GameObject CancelActionPanel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CancelActionPanel.SetActive(false);
+        BuildResidentialAreaBtn.onClick.AddListener(OnBuildAreaCallback);
+        CancelActionBtn.onClick.AddListener(OnCancelActionCallback);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBuildAreaCallback()
+    {
+        CancelActionPanel.SetActive(true);
+        OnBuildAreaHandler?.Invoke();
+    }
+
+    private void OnCancelActionCallback()
+    {
+        CancelActionPanel.SetActive(false);
+        OnCancelActionHandler?.Invoke();
+    }
+
+
+}
